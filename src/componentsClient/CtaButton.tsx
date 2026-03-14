3a9297f51a196f4780f256941e558af5c7b6d709
Leandro Tenorio <leandrotenorio762@gmail.com>
2026-03-14 11:43:08 -0300

    alt

--- src/componentsClient/CtaButton.tsx
+++ src/componentsClient/CtaButton.tsx
@@ -1,5 +1,10 @@
+'use client';
 
 export default function CTAButtons() {
+  const handleClick = () => {
+    window.fbq?.('track', 'InitiateCheckout');
+  };
+
   return (
     <div className="mt-6">
       <div className="flex flex-col items-center justify-center gap-4 sm:flex-row">
@@ -7,6 +12,7 @@ export default function CTAButtons() {
           href="https://checkout.ticto.app/AC9937D1?pid=AF33CF7E4D"
           target="_blank"
           rel="noopener noreferrer"
+          onClick={handleClick}
           className="inline-flex cursor-pointer rounded-2xl bg-green-500 px-3 py-4 mx-2 text-center text-lg md:text-sm font-extrabold uppercase tracking-wide text-white shadow-lg shadow-green-800/100 transition hover:bg-green-700 md:px-16 md:py-6 lg:text-2xl"
         >
           Quero adquirir por menos de R$0,94 por dia
