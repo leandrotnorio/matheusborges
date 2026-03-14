4d837e0b624b36b92db809f8cd2432b141b847b4
Leandro Tenorio <leandrotenorio762@gmail.com>
2026-03-14 11:09:59 -0300

    alt

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -58,6 +58,7 @@ export default function RootLayout({
         'https://connect.facebook.net/en_US/fbevents.js');
         fbq('init', '1624802691854105');
         fbq('track', 'PageView');
+        fbq('track', 'InitiateCheckout');
         `}
       </Script>
       <noscript><img height="1" width="1" style={{display:"none"}}
